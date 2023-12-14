111a686029831e258151422129d0d9fa7783d822
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-12-14 09:45:48 +0530

    speed se push kiya me Folder me

--- sharma.py
+++ sharma.py
@@ -156,6 +156,7 @@ try:
 			new= str(int(b[0]))+":"+b[1]+" AM"
 		if len(new)==7:
 			new="0"+new
+		a=fulltime[3].split(":")
 		text2=f"सभी {shift} के स्टाफ अथवा गॉर्ड अपनी अपनी हाजिरी टाइम स्टांप कैमरे के साथ {new} बजे से पहले जरूर डाल देवे।"
 		scheduler.add_job(first_job, "cron",day_of_week="mon-sun",hour=int(a[0]), minute=int(a[1]), replace_existing=True,args=(text2,) ,id="job2")
 except Exception as e:
