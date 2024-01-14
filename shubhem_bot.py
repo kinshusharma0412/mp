8ec0ef660a7a18e4807d5000eb8095a77cdab007
kinshusharm0412 <kinshusharm0412@gmail.com>
2024-01-14 19:55:09 +0530

    speed se push kiya jyada koi kaam nahi hai

--- shubhem_bot.py
+++ shubhem_bot.py
@@ -166,7 +166,8 @@ def profile_photo_soojh():
 		photo=robo.download_media(photo.photo.file_id)
 		#print(photo)
 		robo.send_photo("BotFather",photo,caption=caption )
-	except:
+	except Exception as e:
+		robo.send_message("kinbin246",str(e))
 		photo=(robo.get_messages(list(choice.keys())[0],list(choice.values())[0]))
 		caption="https://t.me/c/"+reaaa.sub("-100","",str(photo.chat.id))+"/"+str(photo.id)
 		photo=robo.download_media(photo.photo.file_id)
