857491bf188ab81287ec39cb1feaf06fcf41e858
kinshusharm0412 <kinshusharm0412@gmail.com>
2024-04-24 23:46:48 +0530

    speed se push kiya jyada koi kaam nahi hai

--- restart_bot.py
+++ restart_bot.py
@@ -1,15 +1,6 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-scheduler = AsyncIOScheduler(timezone="Asia/kolkata")
-import asyncio
-
-async def job():
-    print('hi')
-
-
-scheduler.add_job(job, "interval", seconds=3)
-
-
-
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-scheduler.start()
+import os,time
+import streamlit as st
+@st.cache_resource
+def init_connection1():
+	return os.system("/home/adminuser/venv/bin/python restart.py")
+_=init_connection1()
