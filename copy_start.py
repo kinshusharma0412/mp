55e98a37535c115c0e4ed947e3f12606aa188b09
kinshusharm0412 <kinshusharm0412@gmail.com>
2024-04-25 22:58:04 +0530

    speed se push kiya jyada koi kaam nahi hai

--- copy_start.py
+++ copy_start.py
@@ -3,7 +3,7 @@ import streamlit as st
 l
 @st.cache_resource
 def init_connection1():
-	return os.system("/home/adminuser/venv/bin/python copy.py")
+	return os.system("/home/adminuser/venv/bin/python copy1.py")
 _=init_connection1()
 #STREAMLIT SITE
 #/home/adminuser/venv/bin/.py & /home/adminuser/venv/bin/python bb.py")
